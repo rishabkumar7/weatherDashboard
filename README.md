3cc53e5880089bd619c6a7761538c4e17797efcf
Rishab Kumar <45825464+rishabkumar7@users.noreply.github.com>
2020-02-21 00:55:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Vue Weather Dashboard
+
+> A Vue.js project
+
+You will need to get your own Google API key to input into index.html.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/WindStatus.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="highlights-item col-md-4 col-sm-6 col-xs-12 border-top">
+    <div>
+    <div class="card-heading pt-5">Wind Status</div>
+    <div class="row pt-4 mt-4">
+      <div class="col-sm-6 col-md-6 mt-2 text-center align-middle">
+        <p class="card-sub-heading mt-3">Wind Direction</p>
+        <p class="mt-4"><img src="../assets/winddirection.svg" height="40" width="40"></p>
+        <p class="card-value mt-4">{{ highlights.windStatus.derivedWindDirection }}</p>
+      </div>
+      <div class="col-sm-6 col-md-6 mt-2">
+        <p class="card-sub-heading mt-3">Wind Speed</p>
+        <p class="mt-4"><img src="../assets/windspeed.svg" height="40" width="40"></p>
+        <p class="card-value mt-4">{{ highlights.windStatus.windSpeed }} km/h</p>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["highlights"],
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+};
+</script>
